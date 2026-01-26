c6f0ef705c7b97ebe1b97f5de1f8db5576fc9f45
JujuJujulio <Juan.DiasDosSantos@VBSDeKlimmuur.onmicrosoft.com>
2026-01-26 11:57:25 +0100

    TEST CHANGE: code om te zien met input parameters

--- HttpTriggerConsume/run.ps1
+++ HttpTriggerConsume/run.ps1
@@ -1,61 +1,40 @@
 using namespace System.Net
 #Import-Module ExchangeOnlineManagement #Mogelijke wijze
 # Input bindings are passed in via param block.
-param($Request, $TriggerMetadata)
+param($Request, $TriggerMetadata) #idk youtube vid met uitleg over param blokken in azure functions powershell
 
-# Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
-
-# define which code path to follow
-#   Exchange:   do the exchange code, cahinging aliases of mailbox
-#   SayHello:   only give a response, returning the parameters
-#   Version:    only return version and configuration info
-$codePath = "Exchange"
-switch ($codePath) {
-    "Version" {
-        $ps_version = $PSVersionTable.PSVersion -join "."
-        $modules = Get-InstalledModule | ForEach-Object { "$($_.Name) ($($_.Version))" }
-        $body = "PSVersion: $ps_version, Modules: $($modules -join ", ")"
-    }
-    "SayHello" { 
-        # Interact with query parameters or the body of the request.
-        $name = $Request.Query.Name
-        if (-not $name) {
-            $name = $Request.Body.Name
-        }
-
-        $body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-        if ($name) {
-            $body = "Hello, $name. This HTTP triggered function executed successfully. CHaaaaaaaange Github"
-        }
-    }
-    "Exchange" {
-        $domain = "VBSDeKlimmuur.onmicrosoft.com"
-        $upn = "rik.hendriks@$domain"
-        $aliasesToAdd = "rh1@$domain", "rh2@$domain"
-        $aliasesToRemove = "rh@$domain"
-    
-        # 1. Add and remove aliases as requested
-        Set-Mailbox $upn -EmailAddresses @{Add=$aliasesToAdd;Remove=$aliasesToRemove}    
-    
-        # 2. Return actual situation
-        $mbox = get-mailbox -Identity $upn
-        if ($null -ne $mbox) {
-            $aliases = $mbox.emailaddresses | Where-Object { $_ -clike "smtp:*"} | ForEach-Object { $_.substring(5) } 
-            $body = "'$upn' aliases: $($aliases -join ", ")"
-        } else {
-            $body = "Error '$upn': mailbox does not exist. $($error[0].exception.message)"
-        }    
-    }
-    Default {
-        $body = "Default codePath"
-    }
+# Input parameters
+#$InputDomain = Request.Query.domain #Line 17 is hardcoded for now
+$InputUpn = Request.Query.upn
+if ($Request.Query.AliasesAdd){
+    $InputAliasesToAdd = Request.Query.AliasesAdd -split "," | ForEach-Object { $_.Trim() }
 }
+if ($Request.Query.AliasesRm){
+    $InputAliasesToRemove = Request.Query.AliasesToRm -split "," | ForEach-Object { $_.Trim() }
+}
+
+# Organization connection settings
+$domain = "VBSDeKlimmuur.onmicrosoft.com"  # $InputDomain
+$upn = "$InputUpn@$domain"
+$aliasesToAdd = $InputAliasesToAdd | ForEach-Object {"$_$domain"}
+$aliasesToRemove = $InputAliasesToRemove | ForEach-Object {"$_$domain"}
+
+# 1. Add and remove aliases as requested
+Set-Mailbox $upn -EmailAddresses @{Add=$aliasesToAdd;Remove=$aliasesToRemove}    
+
+# 2. Return actual situation
+$mbox = get-mailbox -Identity $upn
+if ($null -ne $mbox) {
+    $aliases = $mbox.emailaddresses | Where-Object { $_ -clike "smtp:*"} | ForEach-Object { $_.substring(5) } 
+    $body = "'$upn' aliases: $($aliases -join ", ")"
+} else {
+    $body = "Error '$upn': mailbox does not exist. $($error[0].exception.message)"
+}    
+
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
     Body = $body
 })
-
-#Actuele code
+#From gitBash
